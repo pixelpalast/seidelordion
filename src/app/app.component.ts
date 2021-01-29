5b0ce2c09f7a931ce1331d1b03860423c1af5117
unknown <jonasfroeber@gmail.com>
2021-01-29 10:44:23 +0100

    wc stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <ba-accordion [disabled]="disabled" [label]="label">{{content}}</ba-accordion>
+  `,
 })
 export class AppComponent {
+  public label = "Neuer Artikel 3";
+  public content = 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.';
+  public disabled = true;
+
   title = 'seidelordion';
 }
