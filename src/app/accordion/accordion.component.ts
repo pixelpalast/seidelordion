665b94e184700e7db4aad06bc0bfef6b9ab3e317
Florian Seidel <florian.seidel@huk-coburg.de>
2021-03-23 14:18:18 +0100

    rename all

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,31 +1,17 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
-  styleUrls: ['./accordion.component.css'],
   template: `
-    <ba-accordion [disabled]="disabled" [label]="{{title}}">
+    <ba-accordion [label]="title" [icon]="iconLeft" [color-variant]="variant" [disabled]="disabled">
       <p>{{panelText}}</p>
-    </ba-accordion>
-
-    <div class="{{class}}">
-    <button class="accordion" (click)="toggle()"><div class="icon {{icon}}"></div></button>
-    <div class="panel" #panel>
-      <p>
-        {{panelText}}
-      </p>
-    </div>
-  </div>`
+    </ba-accordion>`
 })
-
 export class AccordionComponent implements OnInit {
-
-  @Input() icon = '';
   @Input() title = '';
   @Input() iconLeft = false;
   @Input() variant = '';
   @Input() disabled = false;
-  @ViewChild("panel", { static: false }) panel: ElementRef;
 
   public panelText = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -37,23 +23,5 @@ export class AccordionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  toggle() {
-    if (!this.disabled) {
-      this.visible = !this.visible;
-      this.panel.nativeElement.style.display = this.visible ? "block" : "none";
-      this.icon = this.visible ? 'minus' : 'plus';
-    }
-  }
-
-  get class() {
-    let variantClass = `accordion-item-${this.variant}`;
-    let returnValue = `accordion-item ${(this.variant) ? variantClass : ''} ${this.disabled ? 'disabled' : ''}`;
-    if (this.iconLeft) {
-      returnValue += ` icon-left`;
-    }
-
-    return returnValue.trim();
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ export class AppComponent {
   public content = 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.';
   public disabled = true;
 
-  title = 'seidelordion';
+  title = 'baaccordiordion';
 }
